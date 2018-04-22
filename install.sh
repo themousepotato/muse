209dce02b0992f63db156ba06e957345175f819c
TheMousePotato <navaneeths1998@gmail.com>
2018-04-22 13:23:22 +0530

    Update

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 sudo mkdir {/usr/share/muse,/usr/share/muse/img}
 sudo cp {muse.py,quotes.txt,LICENSE,README.md} /usr/share/muse
+mkdir {~/.config,~/.config/autostart}
 cp muse.desktop ~/.config/autostart
 cd img
 sudo cp {1.png,2.png,3.png,4.png,5.png,6.png} /usr/share/muse/img
